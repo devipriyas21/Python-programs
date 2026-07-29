088e6c20aa49b535fa7c985ef08bb0ea82c07c0c
Devipriya S <sdevipriya1234@gmail.com>
2026-07-29 23:26:35 +0530

    Add Duck Number Checker script

---
+++ duck_number.py
@@ -0,0 +1,10 @@
+# Duck Number Checker
+
+num = input("Enter a number: ")
+
+if num[0] == '0':
+    print("Not a Duck Number")
+elif '0' in num:
+    print("Duck Number")
+else:
+    print("Not a Duck Number")
